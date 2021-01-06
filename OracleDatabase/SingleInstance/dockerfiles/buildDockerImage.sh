4ec4ac7aaadf89c306b638ddbbe1d7581b4d3da3
kirimaru <nainaistar@gmail.com>
2021-01-06 06:14:11 +0000

    Allow half space include no_proxy

--- OracleDatabase/SingleInstance/dockerfiles/buildDockerImage.sh
+++ OracleDatabase/SingleInstance/dockerfiles/buildDockerImage.sh
@@ -201,7 +201,7 @@ if [ "${ftp_proxy}" != "" ]; then
 fi
 
 if [ "${no_proxy}" != "" ]; then
-  PROXY_SETTINGS="$PROXY_SETTINGS --build-arg no_proxy=${no_proxy}"
+  PROXY_SETTINGS="$PROXY_SETTINGS --build-arg no_proxy=$(echo $no_proxy | sed 's/ //g')"
 fi
 
 if [ "$PROXY_SETTINGS" != "" ]; then
